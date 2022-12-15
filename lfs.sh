3f98e347c0a1c920fd127dc157f5835af8eafd67
WDominic <dom0_o@protonmail.com>
2022-12-14 22:31:16 -0500

    setupdisk

--- lfs.sh
+++ lfs.sh
@@ -3,3 +3,10 @@ export LFS=/mnt/lfs
 export LFS_TGT=x86_64-lfs-linux-gnu 
 export LFS_DISK =/dev/sdb
 
+# check if disk is mounted
+if ! grep -q "$LFS" /proc/mounts; then
+    source setupdisk.sh "$LFS_DISK" 
+    # inherits variables from this file
+    sudo mount "${LFS_DISK}2" "$LFS"
+    sudo chown -v $USER "$LFS"
+fi
