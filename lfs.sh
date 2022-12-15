70515960dcae6c529bc075820b7e03d12e3eea0d
WDominic <dom0_o@protonmail.com>
2022-12-15 11:43:37 -0500

    partitioned disk and created filesystem

--- lfs.sh
+++ lfs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LFS=/mnt/lfs 
 export LFS_TGT=x86_64-lfs-linux-gnu 
-export LFS_DISK =/dev/sdb
+export LFS_DISK=/dev/sdb
 
 # check if disk is mounted
 if ! grep -q "$LFS" /proc/mounts; then
@@ -10,3 +10,20 @@ if ! grep -q "$LFS" /proc/mounts; then
     sudo mount "${LFS_DISK}2" "$LFS"
     sudo chown -v $USER "$LFS"
 fi
+
+mkdir -pv $LFS/tools # for cross compiler
+mkdir -pv $LFS/sources # for tarball source files 
+
+# fsh file system standard
+mkdir -pv $LFS/boot 
+mkdir -pv $LFS/etc
+mkdir -pv $LFS/bin
+mkdir -pv $LFS/lib
+mkdir -pv $LFS/sbin
+mkdir -pv $LFS/usr 
+mkdir -pv $LFS/var 
+
+case $(uname -m) in
+    x86_64) mkdir -pv $LFS/lib64 ;; #check if 64 bit
+esac
+
